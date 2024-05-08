1ae0611b51daf8aa9f046978512d50e3e8f16ccd
Amin Hosseini <Amin Hosseini@DESKTOP-EI0POKD>
2024-05-08 19:46:11 +0330

    Some minor updates.

--- Api/Features/ProductCategories/Queries/GetAllProductCategories/Handler.cs
+++ Api/Features/ProductCategories/Queries/GetAllProductCategories/Handler.cs
@@ -33,8 +33,8 @@ public class Handler : IRequestHandler<Query, IQueryable<GetAllProductCategories
 
         var query = _context.ProductCategories.AsNoTracking();
 
-        if (!query.Any())
-            throw new RecordNotFoundException();
+        //if (!query.Any())
+        //    throw new RecordNotFoundException();
 
         return Task.FromResult(query.ProjectToType<GetAllProductCategoriesDto>());
     }
